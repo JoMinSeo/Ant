fe9f547fefc096d05d1ae5de16ad2080ce9c6095
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-03-02 17:46:06 +0900

    로그 추가

--- Ant_Android/app/src/main/java/com/stock/ant/widget/SingleLIveEvent.kt
+++ Ant_Android/app/src/main/java/com/stock/ant/widget/SingleLIveEvent.kt
@@ -2,7 +2,6 @@ package com.stock.ant.widget
 
 import android.util.Log
 import androidx.annotation.MainThread
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -13,7 +12,7 @@ class SingleLIveEvent<T> : MutableLiveData<T?>() {
 
     override fun observe(owner: LifecycleOwner, observer: Observer<in T?>){
         if(hasActiveObservers()){
-
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
         }
 
         super.observe(owner, Observer { t ->
